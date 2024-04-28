f83cd14dcbe584b29b09d4bde124083eca1e87f8
Vinícius Crisol <vinicius.crisol@hotmail.com>
2024-04-27 21:01:19 -0300

    feat: criação do código base

--- cmd/httpserver/httpserver.go
+++ cmd/httpserver/httpserver.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 
-	"github.com/ViniciusCrisol/minicurso-unifacef-2024/internal/app"
-	"github.com/ViniciusCrisol/minicurso-unifacef-2024/internal/infrastructure/controllers"
-	"github.com/ViniciusCrisol/minicurso-unifacef-2024/internal/infrastructure/persistence"
+	"github.com/ViniciusCrisol/minicurso-unifacef-2024/internal/helpers"
 )
 
+// Para iniciar o programa, execute na pasta raiz do projeto o seguinte comando: go run cmd/httpserver/httpserver.go
 func main() {
-	memURLDAO := persistence.NewMemURLDAO()
-	urlService := app.NewURLService(&memURLDAO)
-	urlController := controllers.NewURLController(&urlService)
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /api/urls", urlController.List)
-	mux.HandleFunc("POST /api/urls", urlController.Save)
-	mux.HandleFunc("GET /{short_code}", urlController.Redirect)
-	http.ListenAndServe(":8080", mux)
+	randomString := helpers.RandomString()
+	fmt.Println(randomString)
 }
